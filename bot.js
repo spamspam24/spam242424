d486c04755da51de0b6784177e752a131ebbd320
spamspam24 <45368983+spamspam24@users.noreply.github.com>
2018-11-27 19:30:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**bot spam by islam سبام بوت **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -76,7 +76,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**bot spam by islam سبام بوت **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
