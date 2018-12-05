969cd3c67ab09fb72938f6e525f63440dcb3e873
spamspam24 <45368983+spamspam24@users.noreply.github.com>
2018-12-05 11:04:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -55,8 +60,114 @@ client2.on('message', message => {
     }
 });
 
+client3.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+       
+      }
+});
+     
+       
+client3.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
 
 
+
+client3.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+client4.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+       
+      }
+});
+     
+       
+client4.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client4.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client5.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+       
+      }
+});
+     
+       
+client5.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client5.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client6.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+       
+      }
+});
+     
+       
+client6.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client6.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client7.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+       
+      }
+});
+     
+       
+client7.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client7.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
       let count = 0;
@@ -85,8 +196,84 @@ if (message.content === '!spam') {
       }
 });
 
+client3.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By islam , Farm Credit By islam, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج  **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By islam , Farm Credit By islam, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج  **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client5.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By islam , Farm Credit By islam, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج  **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client6.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By islam , Farm Credit By islam, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج  **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client7.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By islam , Farm Credit By islam, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج  **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
 
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client3.login(process.env.TOKEN);// لا تغير فيها شيء
+client4.login(process.env.TOKEN);// لا تغير فيها شيء
+client5.login(process.env.TOKEN);// لا تغير فيها شيء
+client6.login(process.env.TOKEN);// لا تغير فيها شيء
+client7.login(process.env.TOKEN);// لا تغير فيها شيء
